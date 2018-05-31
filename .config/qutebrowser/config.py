cb0586e0ed0a33778217765bbb5bb31e6d39b464
Rouji <rj@r0.at>
2018-05-31 04:43:02 +0200

    stuff and things

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -128,39 +128,39 @@ c.downloads.location.directory = '~/Downloads'
 
 # Font used in the completion categories.
 # Type: Font
-c.fonts.completion.category = 'bold 10pt monospace'
+c.fonts.completion.category = 'bold 8pt monospace'
 
 # Font used in the completion widget.
 # Type: Font
-c.fonts.completion.entry = '10pt monospace'
+c.fonts.completion.entry = '8pt monospace'
 
 # Font used for the debugging console.
 # Type: QtFont
-c.fonts.debug_console = '10pt monospace'
+c.fonts.debug_console = '8pt monospace'
 
 # Font used for the downloadbar.
 # Type: Font
-c.fonts.downloads = '10pt monospace'
+c.fonts.downloads = '8pt monospace'
 
 # Font used for the hints.
 # Type: Font
-c.fonts.hints = '10pt monospace'
+c.fonts.hints = '8pt monospace'
 
 # Font used in the keyhint widget.
 # Type: Font
-c.fonts.keyhint = '10pt monospace'
+c.fonts.keyhint = '8pt monospace'
 
 # Font used for error messages.
 # Type: Font
-c.fonts.messages.error = '10pt monospace'
+c.fonts.messages.error = '8pt monospace'
 
 # Font used for info messages.
 # Type: Font
-c.fonts.messages.info = '10pt monospace'
+c.fonts.messages.info = '8pt monospace'
 
 # Font used for warning messages.
 # Type: Font
-c.fonts.messages.warning = '10pt monospace'
+c.fonts.messages.warning = '8pt monospace'
 
 # Default monospace fonts. Whenever "monospace" is used in a font
 # setting, it's replaced with the fonts listed here.
@@ -169,15 +169,15 @@ c.fonts.monospace = '"xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", M
 
 # Font used for prompts.
 # Type: Font
-c.fonts.prompts = '10pt monospace'
+c.fonts.prompts = '8pt monospace'
 
 # Font used in the statusbar.
 # Type: Font
-c.fonts.statusbar = '10pt monospace'
+c.fonts.statusbar = '8pt monospace'
 
 # Font used in the tab bar.
 # Type: QtFont
-c.fonts.tabs = '10pt monospace'
+c.fonts.tabs = '8pt monospace'
 
 # Characters used for hint strings.
 # Type: UniqueCharString
@@ -313,7 +313,7 @@ c.tabs.title.format = '{perc}{title}'
 # Type: FormatString
 c.tabs.title.format_pinned = '{index}'
 
-c.zoom.default = '140%'
+c.zoom.default = '100%'
 
 # Width (in pixels or as percentage of the window) of the tab bar if
 # it's vertical.
--- .screenlayout/default.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --on --primary
