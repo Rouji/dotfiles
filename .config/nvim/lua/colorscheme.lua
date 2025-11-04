10583cdbb619e91725fce45dced72a3aa59aeaf7
Andreas Hackl <a@r0.at>
2025-11-04 19:05:55 +0100

    nvim: background=dark

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -4,7 +4,7 @@ set termguicolors
 let g:gruvbox_contrast_dark='hard'
 let g:gruvbox_contrast_light='hard'
 let g:gruvbox_italic=1
-set background=light
+set background=dark
 colorscheme gruvbox
 highlight VertSplit ctermbg=NONE
 "highlight Normal guibg=NONE ctermbg=NONE
