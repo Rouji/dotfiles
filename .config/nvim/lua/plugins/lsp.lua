0a3c9bf54c020bee040b1b3766e38315d92b4277
Andreas Hackl <a@r0.at>
2026-02-27 17:58:59 +0100

    blah

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,27 +2,27 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "<leader>n", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "<leader>p", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set("n", "<leader>r", function() require('telescope.builtin').lsp_references() end, opts)
+    vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>n", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<leader>p", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>r", function() require('telescope.builtin').lsp_references() end, opts)
 end)
 
 lsp.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
-  servers = {
-    ['null-ls'] = {'python', 'typescript', 'lua'},
-  }
+    format_opts = {
+        async = false,
+        timeout_ms = 10000,
+    },
+    servers = {
+        ['null-ls'] = { 'python', 'typescript', 'lua' },
+    }
 })
 
 lsp.setup()
@@ -45,15 +45,17 @@ require('mason-lspconfig').setup({
         "docker_compose_language_service",
         "pyright",
         "lua_ls",
+        "elmls",
     },
     handlers = {
-      lsp.default_setup,
+        lsp.default_setup,
     },
 })
 
-require('lspconfig').ansiblels.setup({
-    filetypes={"yaml",}
+vim.lsp.config('ansiblels', {
+    filetypes = { "yaml", }
 })
+vim.lsp.enable('ansiblels')
 
 local null_ls = require("null-ls")
 null_ls.setup({
@@ -61,3 +63,4 @@ null_ls.setup({
         null_ls.builtins.formatting.black,
     }
 })
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
